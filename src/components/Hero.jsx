b7814109a1613a39c8a6fd5aee39b24a418e96fd
vikiade00 <vikiade00@gmail.com>
2024-01-19 08:31:03 +0700

    Fix error page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { IMAGES } from "../assets/Image";
 import { ArrowCircleUpRight, Download } from "@phosphor-icons/react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Hero() {
   return (
     <div
       id="home"
-      className="flex items-center  pt-28 md:pt-0  justify-center flex-wrap min-h-[100vh]"
+      className="flex items-center pt-28 md:pt-0 justify-center flex-wrap min-h-[100vh]"
     >
-      <div className=" w-4/5 md:w-2/5">
+      <div className="w-4/5 md:w-2/5">
         <div className="font-bold mt-20 md:mt-0 text-xl md:text-3xl text-gray-700">
           Hi<span className="text-gray-400">,</span> I am Viki Ade Safaat
         </div>
@@ -21,16 +21,16 @@ function Hero() {
           and dynamic user interfaces
         </div>
         <div className="flex gap-7 mt-10 flex-wrap items-center">
-          <Link to="portofolio">
+          <Link to="/portofolio">
             <button className="bg-blue-600 text-white py-2 w-32 flex justify-center items-center gap-2 hover:bg-blue-800">
               Portofolio <ArrowCircleUpRight />
             </button>
           </Link>
-
           <a
-            href="/My Cv.pdf"
-            download="CV Viki Ade S"
-            className="flex items-center gap-2  py-2 w-32 text-gray-900 font-semibold hover:text-blue-800"
+            href="https://drive.google.com/file/d/1T4XMt-EscwQBUTmvrCHFd6jTwXvesbUy/view"
+            className="flex items-center gap-2 py-2 w-32 text-gray-900 font-semibold hover:text-blue-800"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Download CV{" "}
             <span>
